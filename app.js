172e0a73f6d7592e0425de7a4d9bd78ff31bf24f
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-08 16:01:21 +0100

    board appears with squares, no colours

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ const width = 8
 const startPieces = [
   rook, knight, bishop, queen, king, bishop, knight, rook,
   pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-  '', '', '', '', '', '', '', '', '',
-  '', '', '', '', '', '', '', '', '',
-  '', '', '', '', '', '', '', '', '',
-  '', '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
   pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-  rook, knight, bishop, queen, king, bishop, knight, rook,
+  rook, knight, bishop, queen, king, bishop, knight, rook
 ]
+
+function createBoard() {
+  startPieces.forEach((startPiece, i) => {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.setAttribute('square-id', i)
+    square.classList.add('beige');
+    gameBoard.append(square);
+  });
+}
+
+createBoard()
+
