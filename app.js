28beef95fbefd183b4322e4dde22519b05d8ce90
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-11 08:03:01 +0100

    continue to add drag logic

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const gameBoard = document.querySelector("#gameboard")
-const player = document.querySelector("#player")
+const playerDisplay = document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
 const width = 8 
+let playerGo = 'black'
+playerDisplay.textContent = 'black'
 
 const startPieces = [
   rook, knight, bishop, queen, king, bishop, knight, rook,
@@ -19,12 +21,19 @@ function createBoard() {
     const square = document.createElement('div');
     square.classList.add('square');
     square.innerHTML = startPiece
+    square.firstChild?.setAttribute('draggable', true)
     square.setAttribute('square-id', i)
     const row = Math.floor( (63 - i) / 8) + 1
     if (row % 2 === 0) {
-      square.classList.add(i % 2 === 0 ? "white" : "green")
+      square.classList.add(i % 2 === 0 ? "grey" : "green");
     } else {
-      square.classList.add(i % 2 === 0 ? "green" : "white")
+      square.classList.add(i % 2 === 0 ? "green" : "grey");
+    }
+
+    if ( i <= 15 ) {
+      square.firstChild.firstChild.classList.add('black');
+    } else if ( i >= 48 ) {
+      square.firstChild.firstChild.classList.add('white');
     }
 
     square.classList.add("centered");
@@ -33,4 +42,36 @@ function createBoard() {
 }
 
 createBoard()
+
+
+const allSquares = document.querySelectorAll("#gameboard .square");
+
+allSquares.forEach(square => {
+  square.addEventListener('dragstart', dragStart);
+  square.addEventListener('dragover', dragOver)
+  square.addEventListener('drop', dragDrop)
+})
+
+let startPositionId;
+let draggedElement;
+
+function dragStart (e) {
+  startPositionId = e.target.parentNode.getAttribute('square-id');
+  draggedElement = e.target;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop(e) {
+  // stop piece being dropped into two or more squares.
+  e.stopPropagation();
+  const taken = e.target.classList.contains('piece');
+  e.target.parentNode.append(draggedElement);
+  e.target.remove();
+  // e.target.append(draggedElement)
+}
+
+
 
