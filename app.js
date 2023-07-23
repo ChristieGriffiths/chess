84617de417245c4a19a786344beb51cec76680cf
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-23 10:21:49 +0100

    create changeplayer, checkifValid, dragDrop functions

--- app.js
+++ app.js
@@ -66,23 +66,64 @@ function dragOver(e) {
 
 function dragDrop(e) {
   e.stopPropagation()
-  console.log(e.target)
-  const taken = e.target.class.contains('piece')
-  // e.target.parentNode.append(draggedElement)
-  // e.target.remove()
-  // e.target.append(draggedElement)
-  changePlayer()
+  // console.log('playerGo', playerGo)
+  // console.log('e.target', e.target)
+  const correctGo = draggedElement.firstChild.classList.contains(playerGo)
+  const taken = e.target.classList.contains('piece')
+  const valid = checkIfValid(e.target)
+  const opponentGo = playerGo === 'white' ? 'black' : 'white'; 
+  const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo)
+
+  if (correctGo) {
+    // must check this first
+    if (takenByOpponent && valid) {
+      e.target.parentNode.append(draggedElement)
+      e.target.remove()
+      changePlayer()
+      return
+    } 
+    // then check this 
+    if (taken && !takenByOpponent) {
+      return
+    }
+    if (valid) { 
+      e.target.append(draggedElement)
+      changePlayer()
+      return 
+    }
+  }
 }
 
-function Player() {
+function checkIfValid(target) {
+  console.log(target);
+  const targetId = Number(target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'));
+  const startId =  Number(startPositionId);
+  const piece = draggedElement.id
+  console.log(targetId);
+
+}
+
+function changePlayer() {
   if (playerGo === "black") {
-    playerGo = "white"
+    reverseIds()
+    playerGo = "white"  
     playerDisplay.textContent = "white"
   } else {
+    revertIds();
     playerGo = "black"
     playerDisplay.textContent = "black"
   }
 }
 
+function reverseIds() {
+  const allSquares = document.querySelectorAll(".square")
+  allSquares.forEach((square, i) => 
+  square.setAttribute('square-id', (width * width - 1) - i))
+}
+
+function revertIds() {
+  const allSquares = document.querySelectorAll(".square")
+  allSquares.forEach((square, i) => square.setAttribute('square-id', i))
+}
 
 
