ffa46ab2cf21e2198408b427edb187b01ffac1e2
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-07-24 10:08:07 +0100

    finish!

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ function dragDrop(e) {
     if (takenByOpponent && valid) {
       e.target.parentNode.append(draggedElement)
       e.target.remove()
+      checkforWin()
       changePlayer()
       return
     } 
@@ -88,6 +89,7 @@ function dragDrop(e) {
     }
     if (valid) { 
       e.target.append(draggedElement)
+      checkforWin()
       changePlayer()
       return 
     }
@@ -295,6 +297,21 @@ function reverseIds() {
 function revertIds() {
   const allSquares = document.querySelectorAll(".square")
   allSquares.forEach((square, i) => square.setAttribute('square-id', i))
+}
+
+function checkforWin() {
+  const kings = Array.from(document.querySelectorAll('#king'));
+  console.log(kings)
+  if (!kings.some(king => king.firstChild.classList.contains('white'))) {
+    infoDisplay.innerHTML = "Black player wins!"
+    const allSquare = document.querySelectorAll('.square')
+    allSquares.forEach(square => square.firstChild?.setAttribute('draggable', false))
+  }
+  if (!kings.some(king => king.firstChild.classList.contains('black'))) {
+    infoDisplay.innerHTML = "Black player wins!"
+    const allSquare = document.querySelectorAll('.square')
+    allSquares.forEach(square => square.firstChild?.setAttribute('draggable', false))
+  }
 }
 
 
